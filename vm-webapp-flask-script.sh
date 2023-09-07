680d892699700f9207029e4abb1970c36aeaeadf
adikoren <141913501+adikoren@users.noreply.github.com>
2023-09-07 12:51:26 +0300

    Update vm-webapp-flask-script.sh

--- vm-webapp-flask-script.sh
+++ vm-webapp-flask-script.sh
@@ -11,7 +11,7 @@ Flask Flask-SQLAlchemy psycopg2
 apt-get install -y python3-flask python3-itsdangerous python3-pyinotify python3-simplejson python3-werkzeug
 git clone https://github.com/adikoren/tr_progect.git
 # Run the Flask app
-python3 /var/lib/waagent/custom-script/download/0/terraform_project/app.py &
+python3 /var/lib/waagent/custom-script/download/0/tr_progect/flask_app/app.py &
 
 
 
